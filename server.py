9346e782559dc8f76646cfd1016fcdf8c9886b10
sanello1982-a11y <sanello1982@gmail.com>
2025-10-16 01:23:10 +0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,56 +1,51 @@
-# server.py
 from flask import Flask
-from flask_socketio import SocketIO, join_room, send
+from flask_socketio import SocketIO, send, emit
 import os
+import uuid
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'galaxy_secret')
-socketio = SocketIO(app, cors_allowed_origins="*", logger=False, engineio_logger=False)
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 
-# Хранилище комнат: {game_id: {'host': sid, 'client': sid or None, 'state': {...}}}
 games = {}
 
 @socketio.on('create')
 def handle_create():
-    from uuid import uuid4
-    game_id = str(uuid4())[:6].upper()
+    game_id = str(uuid.uuid4())[:6].upper()
     games[game_id] = {'host': request.sid, 'client': None, 'state': None}
-    join_room(game_id)
     print(f"Created game: {game_id}")
-    send({'type': 'created', 'game_id': game_id}, to=request.sid)
+    emit('created', {'game_id': game_id})
 
 @socketio.on('join')
 def handle_join(data):
     game_id = data.get('game_id')
     if game_id in games and games[game_id]['client'] is None:
         games[game_id]['client'] = request.sid
-        join_room(game_id)
-        send({'type': 'joined'}, to=request.sid)
-        send({'type': 'peer_joined'}, to=games[game_id]['host'])
-        print(f"Player joined game: {game_id}")
+        emit('joined')
+        emit('peer_joined', room=games[game_id]['host'])
+        print(f"Player joined: {game_id}")
     else:
-        send({'type': 'error', 'msg': 'Game not found or full'}, to=request.sid)
+        emit('error', {'msg': 'Game not found or full'})
 
 @socketio.on('state')
 def handle_state(data):
     game_id = data.get('game_id')
     if game_id in games:
-        # Рассылаем состояние другому игроку
         target = None
         if request.sid == games[game_id]['host']:
             target = games[game_id]['client']
         elif request.sid == games[game_id]['client']:
             target = games[game_id]['host']
         if target:
-            send({'type': 'state', 'data': data['data']}, to=target)
+            emit('state', data['data'], room=target)
 
 @socketio.on('disconnect')
 def handle_disconnect():
     sid = request.sid
-    for game_id, game in list(games.items()):
+    for gid, game in list(games.items()):
         if game['host'] == sid or game['client'] == sid:
-            print(f"Game {game_id} closed due to disconnect")
-            del games[game_id]
+            print(f"Game {gid} closed")
+            del games[gid]
             break
 
 if __name__ == '__main__':
